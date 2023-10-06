fd185f7cbd9a1e32cbe8f29efea5a215110ffce3
pablo <wademcfild@gmail.com>
2023-10-06 11:19:32 -0300

    ...

---
+++ C#(basic)/2.variables/variables.cs
@@ -0,0 +1,12 @@
+public class Lecture {
+
+    int age = 20
+
+    public static void Main(string[] args)
+    {
+        age = 20; // new value gets assigned
+
+        Console.WriteLine(age) // Output will be 20
+    }
+
+}
