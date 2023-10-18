c31250b2583ffba44445a00396b8f59fb3d5ebe8
pablo <wademcfild@gmail.com>
2023-10-18 11:49:08 -0300

    ...

---
+++ basic/4.numeros/int.cs
@@ -0,0 +1,23 @@
+int a = 18;
+int b = 6;
+int c = a + b;
+Console.WriteLine(c);
+
+// O tipo int representa um inteiro, um número inteiro positivo ou negativo. Você usa o símbolo + para adição. Outras operações matemáticas comuns para inteiros incluem:
+
+// - para subtração
+// * para multiplicação
+// / ou % para divisão 
+
+// Subtração:
+int c = a - b;
+
+// Multiplicação:
+int c = a * b;
+
+// Divisão:
+int c = a / b;
+int c = a % b;
+
+// Adição:
+int c = a + b;
