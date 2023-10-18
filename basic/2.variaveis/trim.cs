6dce0dd3c06bcb1d946cec6c01895b894654061e
pablo <wademcfild@gmail.com>
2023-10-18 10:41:50 -0300

    ...

---
+++ basic/2.variaveis/trim.cs
@@ -0,0 +1,21 @@
+// Suponha que suas cadeias de caracteres têm espaços à esquerda ou à direita que você não deseja exibir. Você deseja cortar os espaços das cadeias de caracteres. O método Trim e os métodos relacionados TrimStart e TrimEnd fazem isso funcionar. Você pode apenas usar esses métodos para remover espaços à esquerda e à direita
+
+
+string greeting = "    hello world!     ";
+Console.WriteLine($"[{greeting}]"); 
+// [    hello world!     ]
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+// [hello world!     ]
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+// [    hello world!]
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
+// [hello world!]
+
+// os [] servem para visualizar melhor a ação do método trim
+// resumindo trim remove espaços, seja do ínicio, fim ou ambos
